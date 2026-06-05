cbd186abcdbac36897f0d54fc02a15622496e61f
Maciej Lewandowski <maciek2002.10@wp.pl>
2026-06-06 01:00:32 +0200

    chore: validate empty tab smoke navigation

--- scripts/androidSmokeDevReleaseEmbedded.mjs
+++ scripts/androidSmokeDevReleaseEmbedded.mjs
@@ -107,6 +107,7 @@ process.env.ANDROID_SMOKE_EXPECT_TEXTS ??= 'Wallets,No wallets,Create new wallet
 process.env.ANDROID_SMOKE_EXPECT_RESOURCE_IDS ??=
   'dashboard-header,no-wallets-icon,create-wallet-button,import-wallet-button,navigation-tab-0';
 process.env.ANDROID_SMOKE_VALIDATE_EMPTY_DASHBOARD_CTAS ??= 'true';
+process.env.ANDROID_SMOKE_VALIDATE_EMPTY_TAB_NAVIGATION ??= 'true';
 process.env.ANDROID_SMOKE_WAIT_MS ??= '45000';
 process.env.ANDROID_SMOKE_CLEAR_APP_DATA ??= 'true';
 
