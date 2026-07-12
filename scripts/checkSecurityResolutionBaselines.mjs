15d5bea4d6261a6d22412a39708f1cdf1432b8db
Maciej Lewandowski <maciek2002.10@wp.pl>
2026-07-12 02:18:36 +0200

    chore: refresh brace expansion security lockfile

--- scripts/checkSecurityResolutionBaselines.mjs
+++ scripts/checkSecurityResolutionBaselines.mjs
@@ -35,6 +35,7 @@ const vulnerableLockEntries = [
   ['ansi-regex', '4.1.0'],
   ['ansi-regex', '5.0.0'],
   ['base-x', '3.0.8'],
+  ['brace-expansion', '1.1.11'],
   ['braces', '3.0.2'],
   ['cipher-base', '1.0.4'],
   ['elliptic', '6.5.4'],
@@ -66,6 +67,7 @@ const vulnerableLockEntries = [
 const expectedLockVersions = new Map([
   ['ansi-regex', ['3.0.1', '4.1.1', '5.0.1', '6.2.2']],
   ['base-x', ['3.0.11', '5.0.1']],
+  ['brace-expansion', ['1.1.16', '2.1.2', '5.0.7']],
   ['braces', ['3.0.3']],
   ['cipher-base', ['1.0.7']],
   ['elliptic', ['6.6.1']],
